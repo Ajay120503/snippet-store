df4daa03ad154db04a45769ce0c0a8bb65609a9c
Ajay120503 <ajaykandhare12@gmail.com>
2026-02-23 19:14:46 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -76,7 +76,7 @@ const Home = () => {
   const SkeletonGrid = () => (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {[...Array(6)].map((_, i) => (
-        <div key={i} className="h-44 rounded-2xl bg-base-200 animate-pulse" />
+        <div key={i} className="h-70 rounded-2xl bg-base-200 animate-pulse" />
       ))}
     </div>
   );
